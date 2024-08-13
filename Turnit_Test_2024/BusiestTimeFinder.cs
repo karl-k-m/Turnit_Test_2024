70ff3a749339ca30e7908900d9a5f64c6e49fa5c
karl-k-m <karl.k.muldia@gmail.com>
2024-08-13 23:27:48 +0300

    added source code

--- Turnit_Test_2024/BusiestTimeFinder.cs
+++ Turnit_Test_2024/BusiestTimeFinder.cs
@@ -1,6 +1,86 @@
+using System.Text.RegularExpressions;
+
 namespace Turnit_Test_2024;
 
-public class BusiestTimeFinder
+public static class BusiestTimeFinder
 {
     
+    /// <summary>
+    /// Use sweep line algorithm to find the busiest interval of time in the bus schedule.
+    /// </summary>
+    /// <param name="events">List of events (2D array of [time: int][+1 / -1])</param>
+    /// <returns></returns>
+    public static (string, int) FindBusiestTime(int[][] events)
+    {
+        // Sweep line algorithm - sort events by start time and iterate through them, keeping track of the current overlap.
+        // An event is either a start or end, so +1 overlap when bus driver starting break, and -1 overlap when bus driver ending break.
+        Array.Sort(events, (a, b) => a[0].CompareTo(b[0]));
+        
+        var maxTotalOverlap = 0;
+        var currentOverlap = 0;
+        var maxStartTime = -1;
+        var maxEndTime = -1;
+
+        for (var e = 0; e < events.Length; e++)
+        {
+            // tempOverlap used because both start and end events are included in the same interval
+            var tempOverlap = currentOverlap;
+            if (events[e][1] > 0) { tempOverlap += 1; }
+            if (tempOverlap > maxTotalOverlap)
+            {
+                maxTotalOverlap = currentOverlap;
+                maxEndTime = events[e][0];
+            }
+            currentOverlap += events[e][1];
+        }
+
+        // Once you reach first element with max overlap, you can be sure that that is the start time of the busiest time
+        currentOverlap = 0;
+        for (var e = 0; e < events.Length; e++)
+        {
+            currentOverlap += events[e][1];
+            if (currentOverlap == maxTotalOverlap)
+            {
+                maxStartTime = events[e][0];
+                break;
+            }
+        }
+        
+        string formattedStartTime = $"{(maxStartTime / 100):D2}:{(maxStartTime % 100):D2}";
+        string formattedEndTime = $"{(maxEndTime / 100):D2}:{(maxEndTime % 100):D2}";
+
+        return ($"{formattedStartTime}-{formattedEndTime}", maxTotalOverlap);
+    }
+    
+    /// <summary>
+    /// Convert time input from hh:mmhh::mm format to 2d int array of events (time, +1/-1 for start/end)
+    /// </summary>
+    /// <param name="input">String input</param>
+    /// <returns></returns>
+    public static int[][] ConvertTimeInput(string input)
+    {
+        // regex pattern for hh:mmhh::mm\n
+        var pattern = @"\d{2}:\d{2}\d{2}:\d{2}(\\n)?";
+        
+        var eventStrings = input.Split('\n');
+        foreach (var line in eventStrings)
+        {
+            if (!Regex.IsMatch(line, pattern))
+            {
+                throw new ArgumentException("Invalid input: " + line);
+            }
+        }
+        
+        var events = new List<int[]>();
+        for (var i = 0; i < eventStrings.Length; i++)
+        {
+            var startTime = int.Parse(eventStrings[i].Substring(0, 5).Replace(":", ""));
+            var endTime = int.Parse(eventStrings[i].Substring(5, 5).Replace(":", ""));
+            
+            events.Add(new int[] {startTime, 1});
+            events.Add(new int[] {endTime, -1});
+        }
+        
+        return events.ToArray();
+    }
 }
--- Turnit_Test_2024/Program.cs
+++ Turnit_Test_2024/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+namespace Turnit_Test_2024;
+
+public class Program
+{
+    public void RunCliLoop(List<string>? presetEvents)
+    {
+        var events = new List<string>();
+        string pattern = @"^\d{2}:\d{2} \d{2}:\d{2}$"; // regex pattern for hh:mm hh:mm
+        
+        // Manual input
+        if (presetEvents != null)
+        {
+            Console.WriteLine("You may add further times:");
+            events.AddRange(presetEvents);
+        }
+        Console.WriteLine("Enter the bus drivers' breaks in the format 'hh:mm hh::mm' (e.g. '09:00 09:30')");
+        Console.WriteLine("Enter 'exit' to quit.");
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "exit") { break; }
+            if (!Regex.IsMatch(input, pattern))
+            {
+                Console.WriteLine("Invalid input. Please enter the time in the format 'hh:mm hh:mm'.");
+                continue;
+            }
+            events.Add(input.Replace(" ", ""));
+            var eventsArray = BusiestTimeFinder.ConvertTimeInput(string.Join("\n", events));
+            var result = BusiestTimeFinder.FindBusiestTime(eventsArray);
+            Console.WriteLine("Busiest time: " + result.Item1 + " Number of drivers taking a break: " + result.Item2);
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            if (args[0] != "-filepath" || args.Length != 2)
+            {
+                Console.WriteLine("Invalid arguments. Use -filepath <path> to specify a file path.");
+            } else
+            {
+                try
+                {
+                    var events = File.ReadAllText(args[1]).Trim();
+                    var eventsArray = BusiestTimeFinder.ConvertTimeInput(events);
+                    var result = BusiestTimeFinder.FindBusiestTime(eventsArray);
+                    Console.WriteLine("Busiest time: " + result.Item1);
+                    Console.WriteLine("Number of buses: " + result.Item2);
+                    new Program().RunCliLoop(events.Split('\n').ToList());
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("File not found. Error: " + e.Message);
+                }
+            }
+        } else
+        {
+            new Program().RunCliLoop(null);
+        }
+    }
+}
